6adb9d2807a6ff54aa40275f77b3d06028f6ebec
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-23 19:55:32 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray([dog], bird) {
+  var array = [dog];
+  array.unshift(bird);
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray([dog], bird) {
+  var array = [dog];
+  array.unshift(bird);
+  return array
+}

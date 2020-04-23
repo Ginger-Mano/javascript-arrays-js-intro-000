13e875f4835dc5d91a7498ec8286a0be25ad4e0d
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-23 21:37:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray([dog], bird) {
-  var array = [dog];
-  array.unshift(bird)
+  array = [element, ...array];
   return array
 }
 

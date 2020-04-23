1788847d2cd244930013dbffe582627956d13028
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-23 22:16:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 function removeElementFromBeginningOfArray(array) {
   array.slice(0);
-  return array.slice
+  return array.slice(0)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {

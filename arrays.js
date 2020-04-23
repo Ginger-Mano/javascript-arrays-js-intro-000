3c41abb8192a694c042c83b438a15b5f68bf7208
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-23 19:57:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,6 @@ function addElementToBeginningOfArray([dog], bird) {
 
 function destructivelyAddElementToBeginningOfArray([dog], bird) {
   var array = [dog];
-  array.unshift(bird);
+  [dog, ...array]
   return array
 }

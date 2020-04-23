5799ccb1791fa926c99c1e3682698509f70b6f32
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-23 21:38:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  array.push(element);
+  array = [element, ...array];
   return array
 }
 
